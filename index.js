e0b6850bbf0a3ad6d8a33f9dee0f5c8509db05dc
xDiel01 <danielzancito@gmail.com>
2025-12-03 16:39:50 -1100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.post('/login', (req, res) => {
     const { usuario, password } = req.body;
 
+    // --- LISTA DE USUARIOS VIP ---
     const usuariosPermitidos = [
         { user: "admin", pass: "diel.1415", nombre: "Admin Supremo" },
         { user: "obed", pass: "papu.12345", nombre: "Obed (Socio)" },
-        { user: "user07", pass: "cash2025", nombre: "Vendedor 01" }
+        { user: "user01", pass: "cash2025", nombre: "Vendedor 1" }
     ];
 
+    // Buscamos si coincide con alguno de la lista
     const usuarioEncontrado = usuariosPermitidos.find(u => u.user === usuario && u.pass === password);
 
     if (usuarioEncontrado) {
-        
         res.json({ 
             exito: true, 
             mensaje: `¡Bienvenido ${usuarioEncontrado.nombre}!` 
         });
     } else {
-        
         res.json({ exito: false, mensaje: "Credenciales incorrectas." });
     }
+});
+
+// Configuración del puerto para Render
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`);
 });
